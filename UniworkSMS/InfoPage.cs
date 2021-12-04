696c18d6ea57e10244cecf5bfa804dad0820ac46
Faxriddin Xushnazarov <xushnazarov1555@outlook.com>
2021-12-04 11:45:37 +0500

    Bekorchilik

---
+++ UniworkSMS/InfoPage.cs
@@ -0,0 +1,66 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Webkit;
+
+namespace UniworkSMS
+{
+    [Activity(Label = "InfoPage")]
+    public class InfoPage : Activity
+    {
+        string url;
+        WebView webView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.info);
+            // Create your application here
+            var UiDesign = FindViewById<RelativeLayout>(Resource.Id.UiDesignDiv);
+            var Information = FindViewById<RelativeLayout>(Resource.Id.InformationBtn);
+            var UXdesign = FindViewById<RelativeLayout>(Resource.Id.UxDesignBtn);
+            var FounBtn = FindViewById<RelativeLayout>(Resource.Id.FoundationBtn);
+            UiDesign.Click += delegate
+            {
+                url = "https://t.me/The_sardorb3k";
+                var uri = Uri.Parse(url);
+                var intent = new Intent(Intent.ActionView, uri);
+                StartActivity(intent);
+            };
+            UXdesign.Click += delegate
+            {
+                url = "https://t.me/Xushnazarov_555";
+                var uri = Uri.Parse(url);
+                var intent = new Intent(Intent.ActionView, uri);
+                StartActivity(intent);
+            };
+            Information.Click += delegate
+            {
+                StartActivity(typeof(AboutPage));
+            };
+            FounBtn.Click += delegate
+            {
+                Context context = Application.Context;
+                string text = "Developers: Faxriddin Xushnazarov and Sardorbek Sattorov";
+                ToastLength duration = ToastLength.Short;
+                var toast = Toast.MakeText(context, text, duration);
+                toast.Show();
+            };
+        }
+
+        
+
+        private void Information_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ UniworkSMS/MainActivity.cs
@@ -0,0 +1,111 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Webkit;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+using Android.Views;
+using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace UniworkSMS
+{
+    [Activity(Icon = "@drawable/icon", Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        static int count = 0;
+        static string Login = String.Empty, Misol;
+        string Url = "https://uniwork.buxdu.uz/forgot.asp";
+        WebView web_view;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+            web_view = FindViewById<WebView>(Resource.Id.webView);
+            web_view.Settings.JavaScriptEnabled = true;
+            web_view.SetWebViewClient(new HelloWebViewClient());
+            web_view.LoadUrl(Url);
+            Timer timer = new Timer(1000);
+            timer.Elapsed += async (sender, e) => Send(null, null);
+            var StartBtn = FindViewById<Button>(Resource.Id.StartBtn);
+            var InfoBtn = FindViewById<Button>(Resource.Id.InfoBtn);
+            var LoginTxt = FindViewById<EditText>(Resource.Id.logintxt);
+            var CountTxt = FindViewById<TextView>(Resource.Id.countTxt);
+
+            // Information Page
+            var InformationBtn = FindViewById<Button>(Resource.Id.InformationBtn);
+            var UxDesignBtn = FindViewById<TextView>(Resource.Id.UxDesignBtn);
+            if (LoginTxt.Text == "")
+            {
+                StartBtn.Enabled = false;
+            }
+            else
+            {
+                StartBtn.Enabled = true;
+            }
+            web_view.EvaluateJavascript("document.getElementsByTagName('div')[12].innerHTML='';", null);
+            LoginTxt.TextChanged += (s, e) =>
+            {
+                CountTxt.Text = "0";
+                count = 0;
+                if (LoginTxt.Text == "")
+                {
+                    StartBtn.Enabled = false;
+                }
+                else
+                {
+                    StartBtn.Enabled = true;
+                }
+            };
+            InfoBtn.Click += (s, e) =>
+            {
+                //SetContentView(Resource.Layout.info);
+                StartActivity(typeof(InfoPage));
+            };
+            StartBtn.Click += (s, e) =>
+            {
+                count++;
+                CountTxt.Text = count.ToString();
+                Login = LoginTxt.Text;
+                web_view.EvaluateJavascript($"document.getElementById('login').value ='{Login}';", null);
+                web_view.EvaluateJavascript(@"var natija=document.getElementsByTagName('tr')[4].innerText.replace('=\n\t\t','');
+document.getElementById('javob').value = eval(natija);", null);
+                web_view.EvaluateJavascript("document.getElementsByTagName('input')[5].form.submit();", null);
+
+            };
+            static async void Send(object s, EventArgs e)
+            {
+
+
+            }
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            Toast.MakeText(this, "Action selected: " + item.TitleFormatted,
+                ToastLength.Short).Show();
+            return base.OnOptionsItemSelected(item);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        
+    }
+    public class HelloWebViewClient : WebViewClient
+    {
+        [System.Obsolete]
+        public override bool ShouldOverrideUrlLoading(WebView view, string url)
+        {
+            view.LoadUrl(url);
+            return false;
+        }
+    }
+}
